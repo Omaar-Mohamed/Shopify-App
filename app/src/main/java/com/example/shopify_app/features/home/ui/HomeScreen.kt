37fe04118491ce8f3ce7e14b8f03ff04669b3633
Omaar-Mohamed <102541769+Omaar-Mohamed@users.noreply.github.com>
2024-06-08 20:38:08 +0300

    adding shimmer effect

--- app/src/main/java/com/example/shopify_app/features/home/ui/HomeScreen.kt
+++ app/src/main/java/com/example/shopify_app/features/home/ui/HomeScreen.kt
@@ -1,6 +1,13 @@
 package com.example.shopify_app.features.home.ui
 
 import android.os.Build
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +15,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +28,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +38,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shopify_app.R
 import androidx.navigation.NavHostController
+
 import com.example.shopify_app.core.networking.ApiState
 import com.example.shopify_app.core.networking.AppRemoteDataSourseImpl
 import com.example.shopify_app.core.widgets.ProductCard
@@ -37,6 +49,7 @@ import com.example.shopify_app.features.home.data.repo.HomeRepoImpl
 import com.example.shopify_app.features.home.viewmodel.HomeViewModel
 import com.example.shopify_app.features.home.viewmodel.HomeViewModelFactory
 //import com.example.shopify_app.features.products.ui.FakeProduct
+
 
 
 @Composable
@@ -70,8 +83,47 @@ fun LoadingView() {
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        CircularProgressIndicator()
+        ShimmerEffect()
     }
+}
+
+
+fun Modifier.shimmer(): Modifier = this.then(
+    Modifier.background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color.LightGray.copy(alpha = 0.6f),
+                Color.LightGray.copy(alpha = 0.2f),
+                Color.LightGray.copy(alpha = 0.6f)
+            ),
+            start = Offset(0f, 0f),
+            end = Offset(200f, 200f)
+        ),
+        shape = RoundedCornerShape(16.dp)
+    )
+)
+
+@Composable
+fun ShimmerEffect() {
+    val transition = rememberInfiniteTransition()
+    val shimmerTranslateAnim by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1000f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 1200,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    Box(
+        modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+            .shimmer()
+    )
 }
 
 @Composable
