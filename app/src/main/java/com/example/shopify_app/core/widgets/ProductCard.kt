34f146a37b08e8cb8db52671902f5f1ee0e418da
Omaar-Mohamed <102541769+Omaar-Mohamed@users.noreply.github.com>
2024-06-24 03:43:43 +0300

    product money symbol

--- app/src/main/java/com/example/shopify_app/core/widgets/ProductCard.kt
+++ app/src/main/java/com/example/shopify_app/core/widgets/ProductCard.kt
@@ -122,7 +122,7 @@ fun ProductCard(
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
-                text = priceValue ?: "",
+                text = "$priceValue$",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black,
